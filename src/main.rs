c74aea44ccecf51351bc2b299ece60e685818cab
nonneb <b.pickette@gmail.com>
2017-09-28 12:38:10 +0200

    actually added the right file this time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let arg1:f64 = args[1].parse().unwrap();
+    let arg2:f64 = args[2].parse().unwrap();
+    let arg3:f64 = args[3].parse().unwrap();
+    let arg4:f64 = args[4].parse().unwrap();
+
+    println!("{:?}", args);
+    println!("{}, {}, {}, {}", arg1, arg2, arg3, arg4);
+
 }
